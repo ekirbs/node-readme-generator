d6f634afc58719f21c093651d74b82776b6ae1a5
Eric K <erickirberger@gmail.com>
2022-12-16 21:04:28 -0500

    basic questions working, readme is being generated, projectLicense is undefined when I put it through the other functions...

--- index.js
+++ index.js
@@ -1,99 +1,88 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-// const questions = [];
+const questions = [
+      // what do you want this file name to be?  Or just make it stock...
+      // should all of these be 'else N/A, or just else GONE?
+      {
+        type: 'input',
+        message: 'Enter your project title:',
+        name: 'projectTitle'
+        // validate: (value) => { if(value){return true} else {return 'You must enter a project title:'}}
+      },
+      {
+        type: 'input',
+        message: 'Write a short description of your project:',
+        name: 'projectDescription'
+      },
+      {
+        type: 'input',
+        message: 'Provide installation instructions:',
+        name: 'projectInstallation'
+      },
+      {
+        type: 'input',
+        message: 'Provide usage instructions:',
+        name: 'projectUsage'
+      },
+      {
+        type: 'input',
+        message: 'List your collaborators',
+        name: 'projectCredits'
+      },
+      {
+        type: 'list',
+        message: 'Choose what kind of license should your project have:',
+        name: 'projectLicense',
+        choices: [
+          "Apache_2.0",
+          "ISC",
+          "MIT",
+          "MPL_2.0",
+          "AGPL_v3",
+          "GPLv3",
+          "Unlicense",
+          "N/A"
+        ]
+      },
+      // following sections should be optional.  if nothing is entered and the person okays the prompt warning, the section is removed.
+      {
+        type: 'input',
+        message: 'List the features of your project:',
+        name: 'projectFeatures'
+      },
+      {
+        type: 'input',
+        message: 'Provide contribution guidelines:',
+        name: 'projectGuidelines'
+      },
+      {
+        type: 'input',
+        message: 'Provide test instructions:',
+        name: 'testInstructions'
+      },
+      {
+        type: 'input',
+        message: 'Enter your Github username:',
+        name: 'userGithub'
+      },
+      {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'userEmail'
+      }
+    ]
 
-inquirer.prompt(
-  [
+function init() {
+  inquirer.prompt(questions)
+  .then (data => {
+    console.table(data);
+    const readme = generateMarkdown(data);
+    fs.writeFile('./output/README.md', readme, (err) => {
+      err ? console.error(err) : console.log('readme created.')
+    })
+  })
+}
 
-    // what do you want this file name to be?  Or just make it stock...
-    // fs.writeFile first argument is how to write to
-    {
-      type: 'input',
-      message: 'Enter your project title:',
-      name: 'projectTitle',
-      validate: (value) => { if(value){return true} else {return 'You must enter a project title:'}}
-    },
-    {
-      type: 'input',
-      message: 'Write a short description of your project:',
-      name: 'projectDescription',
-      
-    },
-    //  Do you want a table of contents?
-    {
-      type: 'input',
-      message: 'Provide installation instructions:',
-      name: 'projectInstallation',
-    },
-    {
-      type: 'input',
-      message: 'Provide usage instructions:',
-      name: 'usage',
-    },
-    {
-      type: 'checkbox',
-      message: 'Choose what kind of license should your project have:',
-      name: 'license',
-      choices: [
-        'Apache',
-        "ISC",
-        "MIT",
-        "N/A",
-      ]
-    },
-    {
-      type: 'input',
-      message: 'Provide contribution guidelines:',
-      name: 'guidelines',
-    },
-    {
-      type: 'input',
-      message: 'Provide test instructions:',
-      name: 'test',
-    },
-    {
-      type: 'input',
-      message: 'Enter your Github username:',
-      name: 'username',
-    },
-    {
-      type: 'input',
-      message: 'Enter your email address:',
-      name: 'email',
-    }
-  ]
-)
-  .then((response) => {
-    console.log('response: ', response);
-    // return response.confirm === response.password
-    //   ? console.log('Success!')
-    //   : console.log('You forgot your password already?!'
-    //   )
-    }
-  );
-
-// TODO: Create a function to write README file
-// function writeToFile (fileName, data) {
-//   fs.writeFile('./generateReadme.md', JSON.stringify(data, null, '\t'), err => {
-//     if (err) {
-//       return console.log(err);
-//     }
-    
-//     console.log ('Success!  Youre README file has been generated.')
-//   });
-// }
-
-// function writeToFile(fileName, data) {
-//   fs.writefile('./generateReadme.md', JSON.stringify(data, null, '\t'), err) => {
-//   // generateMarkdown(data), (err, success) => (stuff))
-// }
-
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  // [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+function renderLicenseBadge(projectLicense) {
+  if (projectLicense !== 'N/A') {
+    return `![License: ${projectLicense}](https://img.shields.io/badge/License-${projectLicense}-blue.svg)`;
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(projectLicense) {
+  if (projectLicense !== 'N/A') {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  } else if (projectLicense === 'ISC') {
+    return `(https://opensource.org/licenses/ISC)`;
+  } else if (projectLicense === 'MIT') {
+    return `(https://opensource.org/licenses/MIT)`;
+  } else if (projectLicense === 'MPL_2.0') {
+    return `(https://opensource.org/licenses/MPL-2.0)`;
+  } else if (projectLicense === 'AGPL_v3') {
+    return `(https://www.gnu.org/licenses/agpl-3.0)`;
+  } else if (projectLicense === 'GPLv3') {
+    return `(https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (projectLicense === 'Unlicense') {
+    return `(http://unlicense.org/)`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(projectLicense) {
+  if (projectLicense !== 'N/A') {
+    return `
+  ## License
 
-// TODO: Create a function to generate markdown for README
+  This application is covered by the ${projectLicense} license.
+  `
+  } else {
+    return ``
+  }
+}
+
+// let licenseBadge = renderLicenseBadge();
+// let licenseLink = renderLicenseLink();
+// let licenseSection = renderLicenseSection();
+
 function generateMarkdown(data) {
-  return`# ${data.title}
+  return`# ${data.projectTitle}
+  
+  ![License: ${data.projectLicense}](https://img.shields.io/badge/License-${data.projectLicense}-blue.svg)
+  ${renderLicenseBadge()}${renderLicenseLink()}
 
   ## Description
   
-  ${data.description}
+  ${data.projectDescription}
   
   ## Table of Contents
 
@@ -29,48 +67,33 @@ function generateMarkdown(data) {
   
   ## Installation
   
-  ${installation}
+  ${data.projectInstallation}
   
   ## Usage
   
-  ${usage}
+  ${data.projectUsage}
   
-  ![alt text](assets/images/screenshot.png)
-    
   
   ## Credits
   
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
-  
-  ## License
-  
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-  
-  ---
-  
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-  
+  ${data.projectCredits}
+  ${renderLicenseSection()}
   ## Features
   
-  If your project has a lot of features, list them here.
+  ${data.projectFeatures}
   
   ## How to Contribute
   
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  ${data.projectGuidelines}
   
   ## Tests
   
-  Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+  ${data.testInstructions}
+  
+  ## Questions
+  
+  Github: [${data.userGithub}](https://github.com/${data.userGithub})
+  Email: [${data.userEmail}](mailto:user@example.com)`
 
 }
 
